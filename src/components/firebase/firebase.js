6bbea296dc0e6c75979139cb75e1132cc2e27c34
Aanchal <aanchalmittal523@gmail.com>
2023-05-11 18:05:00 +0530

    first commit

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -8,14 +8,14 @@ import 'firebase/database'
 
 
 var firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+    apiKey: "AIzaSyBh8P2a3lPGtP8cG9SZTEfW1_Z0zeiv9zg",
+    authDomain: "recipeapp-aa890.firebaseapp.com",
+    databaseURL: "https://recipeapp-aa890-default-rtdb.firebaseio.com",
+    projectId: "recipeapp-aa890",
+    storageBucket: "recipeapp-aa890.appspot.com",
+    messagingSenderId: "926799810849",
+    appId: "1:926799810849:web:6e49e249afc906e3e7ee60",
+    measurementId: "G-TQ1GBN8NEB"
 };
 
 firebase.initializeApp(firebaseConfig)
